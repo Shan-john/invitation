4532d91b5ca3e5e979205b7521340a296614cb1c
Shan John <shanjohn5308@gmail.com>
2024-01-02 18:02:19 +0530

    up

--- lib/presentation/Sections/section3.dart
+++ lib/presentation/Sections/section3.dart
@@ -37,11 +37,11 @@ class Section3 extends StatelessWidget {
             style: TextStyle(fontSize: 45, color: Colors.white),
             textAlign: TextAlign.center,
           ),
-          VenueLink("Home", "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d246.04558100042217!2d76.8667091!3d9.3572059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0641fdf32f1935%3A0xfca585c8b7c91f3b!2sShan%20John!5e0!3m2!1sen!2sin!4v1704198414569!5m2!1sen!2sin"),
+          VenueLink("Home", "https://maps.app.goo.gl/DSHkCzfTCc1dSWmcA"),
           Gap(20),
           VenueLink("Church", "https://maps.app.goo.gl/29J6VtXBtKWXYA1S9"),
           Gap(20),
-          VenueLink("Church", "https://goo.gl/maps/R39Z1GBjP5iwhr699"),
+          VenueLink("Church", "https://maps.app.goo.gl/29J6VtXBtKWXYA1S9"),
         ],
       ),
     );
