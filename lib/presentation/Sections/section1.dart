51b61ed9b01968037a80abee32cfe910acd1225e
Shan John <shanjohn5308@gmail.com>
2024-01-06 21:47:17 +0530

    up

--- lib/presentation/Sections/section1.dart
+++ lib/presentation/Sections/section1.dart
@@ -10,21 +10,42 @@ class Section1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.network(
-          "https://i.pinimg.com/564x/ef/62/5c/ef625cfde997b24a0188076813a85545.jpg",
-           
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleContainer("Date", "15"),
-            CircleContainer("Month", "july"),
-            CircleContainer("Year", "2024"),
-          ],
-        ),
-      ],
+    return Container(
+      
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.network(
+            "https://i.pinimg.com/564x/ef/62/5c/ef625cfde997b24a0188076813a85545.jpg",
+             
+          ),
+      Container(
+        alignment: Alignment.center,
+        height: 300,
+        width: 350,
+        decoration: BoxDecoration(image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage( "assests/savethedate.png" )
+        )),
+        
+      
+      ),
+          Positioned(
+            bottom: 0,
+          
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleContainer("Date", "15"),
+                CircleContainer("Month", "july"),
+                CircleContainer("Year", "2024"),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
